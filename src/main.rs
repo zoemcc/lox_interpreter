23040a0eacff511a5dd0df837c69f309de3336d3
Zoe McCarthy <zoemccarthy12@gmail.com>
2020-07-03 14:05:38 -0500

    Initial command line program for taking in a file or not.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
     println!("Hello, world!");
+    let matches = App::new("Lox Interpreter")
+        .version("0.0.0")
+        .author("Zoe McCarthy <zoemccarthy12@gmail.com>")
+        .about("An interpreter for the Lox Programming Language based off of the book Crafting Interpreters by Bob Nystrom <craftinginterpreters.com>")
+        .arg(Arg::with_name("input_file")
+            .help("Sets an input file to interpret")
+            .index(1))
+        .get_matches();
+    if let Some(input_file) = matches.value_of("input_file") {
+        println!("Interpreting input file: {}", input_file);
+    }
+    else {
+        println!("Not using any input file, going into interactive mode.");
+    }
+    println!("Goodbye, world!");
 }
